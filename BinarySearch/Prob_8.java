e85f6ad33791eac1ac1de31b1246988e85baaca7
garvitmalik-123 <garvitmalik275@gmail.com>
2026-06-28 12:59:40 +0530

    Create Prob_8.java
    Patternwise binary search

---
+++ BinarySearch/Prob_8.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+// Find peak in mountain array
+
+public class Prob_8 {
+    public static void main(String[] args) {
+        int arr[]={1,2,4,7,9,6,3};
+
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        System.out.println(start);
+
+    }
+}
